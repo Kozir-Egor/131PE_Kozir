8ac75b5b6ee8cb4506c6b109ed6accb2c8b8be41
Kozir_Egor <wok9@yandex.ru>
2016-02-08 10:20:18 +0300

    задание 1 процентное соотношение

--- 131PE_Kozir/Program.cs
+++ 131PE_Kozir/Program.cs
@@ -10,6 +10,20 @@ namespace _131PE_Kozir
     {
         static void Main(string[] args)
         {
+            int x;
+            int y;
+            Console.WriteLine("Введите сумму дневной выручки");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x > 10000)
+            {
+                y = (x - 10000) / 100 * 10 + 500;
+            }
+            else
+            {
+                y = (x / 100) * 5;
+            }
+            Console.WriteLine("Ваш дневной заработок составляет {0}", y);
+            Console.ReadKey();
         }
     }
 }
