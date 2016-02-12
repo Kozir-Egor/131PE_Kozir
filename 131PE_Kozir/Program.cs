48bf659673d1e5cb12680e25119f9ff5ae65877e
Kozir_Egor <wok9@yandex.ru>
2016-02-12 15:03:51 +0300

    вывод заработка по фамилии из 1.txt ФАМИЛИЮ ПИСАТЬ НА АНГЛИЙСКОМ!!!!!!!!!!!!!!!

--- 131PE_Kozir/Program.cs
+++ 131PE_Kozir/Program.cs
@@ -9,38 +9,38 @@ namespace _131PE_Kozir_3
 {
     class Program
     {
-        static int[] Array(int[] array)
+        static int[] Array(int[] mas)
         {
 
-            int[] p = new int[array.Length];
+            int[] p = new int[mas.Length];
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < mas.Length; i++)
             {
                 if (i > 4)
                 {
-                    if (array[i] <= 100000)
+                    if (mas[i] <= 100000)
                     {
-                        p[i] = array[i] / 100 * 15;
+                        p[i] = mas[i] / 100 * 15;
                     }
-                    if (array[i] > 10000)
+                    if (mas[i] > 10000)
                     {
-                        p[i] = ((array[i] - 10000) / 100 * 10 + 500) * 2;
+                        p[i] = ((mas[i] - 10000) / 100 * 10 + 500) * 2;
                     }
                     else
                     {
-                        p[i] = (array[i] / 100) * 5;
+                        p[i] = (mas[i] / 100) * 5;
                     }
-                    
+
                 }
                 else
                 {
-                    if (array[i] > 10000)
+                    if (mas[i] > 10000)
                     {
-                        p[i] = (array[i] - 10000) / 100 * 10 + 500;
+                        p[i] = (mas[i] - 10000) / 100 * 10 + 500;
                     }
                     else
                     {
-                        p[i] = (array[i] / 100) * 5;
+                        p[i] = (mas[i] / 100) * 5;
                     }
 
                 }
@@ -56,26 +56,87 @@ namespace _131PE_Kozir_3
         {
 
             int i = 0;
-            int[] t;
-            Console.WriteLine("ВВедите название файла");
+            int[] r;
+            Sot[] array = new Sot[3];
+            Console.WriteLine("Введите название файла");
             string name = Console.ReadLine();
-            string[] mas = File.ReadAllText(name).Split(new char[]{' '});
-            int[] array = new int[mas.Length];
-            for(i = 0; i < mas.Length; i++)
+            Console.WriteLine("Введите фамилию сотрудника");
+            string FIO = Console.ReadLine();
+            using (StreamReader reader = new StreamReader(name, Encoding.Default))
             {
-               array[i] = Int32.Parse(mas[i]);
+                for (i = 0; i < 3; i++)
+                {
+                    array[i] = new Sot(reader.ReadLine().Split('|'));
+                    string[] mas = File.ReadAllText(name).Split(new char[] { ' ' });
+                }
             }
-            t = Array(array);
-          //for (i = 0; i < t.Length; i++ )                //для вывода полученного массива!
-          // {
-          //     Console.Write(t[i]);
-          //      Console.Write(' ');
-          // }
-            Console.WriteLine("Заработок равен: {1} ", t.Sum());
+            if (!Sot.CheckFIO(FIO, array))
+            {
+                Console.WriteLine("не найдено");
+            }
+            else
+                foreach (Sot s in array)
+                    if (s.FIO == FIO)
+                        
+                        s.Show();
+
+            //string[] mas = File.ReadAllText(name).Split(new char[]{' '});
+            // int[] array = new int[mas.Length];
+            // for(i = 0; i < mas.Length; i++)
+            // {
+            //   array[i] = Int32.Parse(mas[i]);
+            // }
+            // t = Array(array);
+            //for (i = 0; i < t.Length; i++ )                //для вывода полученного массива!
+            // {
+            //     Console.Write(t[i]);
+            //      Console.Write(' ');
+            // }
+            //Console.WriteLine("Заработок равен: {1} ", r.Sum());
 
             Console.ReadKey();
         }
+        struct Sot
+        {
+            public string FIO;
+            public string rur;
+
+            public Sot(string[] args)
+            {
+                FIO = args[0];
+                rur = args[1];
+            }
 
 
+            static public bool CheckFIO(string FIO, Sot[] array)
+            {
+                bool temp = false;
+                foreach (Sot s in array)
+                    if (s.FIO == FIO)
+                        temp = true;
+                return temp;
+            }
+            public void Show()
+            {
+                int i = 0;
+                //Console.WriteLine("\nФИО: {0} и заработок {1}", FIO, rur);
+                string[] mas = rur.Split(new char[] { ' ' });
+                int[] array = new int[mas.Length];
+                for (i = 0; i < mas.Length; i++)
+                {
+                    array[i] = Int32.Parse(mas[i]);
+                    //Console.WriteLine(mas[i]);
+                }
+                int[] t;
+                t = Array(array);
+                //for (i = 0; i < t.Length; i++)
+                //{
+                //    Console.Write(t[i]);
+                //    Console.Write(' ');
+                //}
+                int sum = t.Sum();
+                Console.WriteLine("Сумма заработка сотрудника {0}: {1} ", FIO, sum);
+            }
+        }
     }
 }
